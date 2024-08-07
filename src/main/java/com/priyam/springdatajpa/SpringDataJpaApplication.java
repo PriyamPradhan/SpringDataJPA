f32312fa196ba181c8d6a7219915972408d0eee3
Priyam Pradhan <priyampradhan07@gmail.com>
2024-08-08 01:19:41 +0530

    create data

---
+++ src/main/java/com/priyam/springdatajpa/SpringDataJpaApplication.java
@@ -0,0 +1,31 @@
+package com.priyam.springdatajpa;
+
+import com.priyam.springdatajpa.model.Game;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(SpringDataJpaApplication.class, args);
+		Game game1 = context.getBean(Game.class);
+		Game game2 = context.getBean(Game.class);
+		Game game3 = context.getBean(Game.class);
+
+		game1.setTag(1);
+		game1.setTitle("AC Mirage");
+		game1.setRating(9.1);
+
+		game2.setTag(2);
+		game2.setTitle("PUBG");
+		game2.setRating(10.0);
+
+		game3.setTag(3);
+		game3.setTitle("Black Flag");
+		game3.setRating(8.2);
+
+	}
+
+}
---
+++ src/main/java/com/priyam/springdatajpa/model/Game.java
@@ -0,0 +1,49 @@
+package com.priyam.springdatajpa.model;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class Game {
+
+    private int tag;
+    private String title;
+    private double rating;
+
+
+
+    public int getTag() {
+        return tag;
+    }
+
+    public void setTag(int tag) {
+        this.tag = tag;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "tag=" + tag +
+                ", title='" + title + '\'' +
+                ", rating=" + rating +
+                '}';
+    }
+}
