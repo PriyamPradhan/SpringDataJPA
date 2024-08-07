a4c949eba1d85cd6809e71982f7af4100ddcd912
Priyam Pradhan <priyampradhan07@gmail.com>
2024-08-08 01:23:42 +0530

    create game repo + new DB - GameJPA

---
+++ src/main/java/com/priyam/springdatajpa/repository/GameRepo.java
@@ -0,0 +1,10 @@
+package com.priyam.springdatajpa.repository;
+
+import com.priyam.springdatajpa.model.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameRepo extends JpaRepository<Game, Integer> {
+
+}
